17eed9fb8bbf0e32b2619c4ab9e9f64b3c36bce5
Vladimir Marek <vlmarek@volny.cz>
2013-04-09 22:45:30 +0200

    Replace GNU extension ##__VA_ARGS__ by portable substitution
    If you have macro
     #define eprintf(format, ...) fprintf (stderr, format, ##__VA_ARGS__)
    the extension '##' removes preceeding comma ',' if __VA_ARGS__ is empty.
    This is however not portable.
    
    The fix uses portable solution described by Richard Hansen:
    http://stackoverflow.com/questions/5588855/standard-alternative-to-gccs-va-args-trick

--- notmuch-client.h
+++ notmuch-client.h
@@ -97,14 +97,38 @@ typedef struct notmuch_show_params {
  * bogus value).
  *
  * Note that __location__ comes from talloc.h.
+ *
+ * This uses modified Richard Hansen's standards compliant variadic
+ * macro trick
+ * http://stackoverflow.com/questions/5588855/standard-alternative-to-gccs-va-args-trick
  */
-#define INTERNAL_ERROR(format, ...)			\
-    do {						\
-	fprintf(stderr,					\
-		"Internal error: " format " (%s)\n",	\
-		##__VA_ARGS__, __location__);		\
-	exit (1);					\
+#define INTERNAL_ERROR(...)					\
+    do {							\
+	fprintf(stderr,						\
+		"Internal error: " FIRST(__VA_ARGS__) " (%s)\n"	\
+		REST(__VA_ARGS__), __location__);		\
+	exit (1);						\
     } while (0)
+
+/* expands to the first argument */
+#define FIRST(...) FIRST_HELPER(__VA_ARGS__, throwaway)
+#define FIRST_HELPER(first, ...) first
+
+/*
+ * if there's only one argument, expands to nothing.  if there is more
+ * than one argument, expands to a comma followed by everything but
+ * the first argument.  only supports up to 9 arguments but can be
+ * trivially expanded.
+ */
+#define REST(...) REST_HELPER(NUM(__VA_ARGS__), __VA_ARGS__)
+#define REST_HELPER(qty, ...) REST_HELPER2(qty, __VA_ARGS__)
+#define REST_HELPER2(qty, ...) REST_HELPER_##qty(__VA_ARGS__)
+#define REST_HELPER_ONE(first)
+#define REST_HELPER_TWOORMORE(first, ...) , __VA_ARGS__
+#define NUM(...) \
+        SELECT_10TH(__VA_ARGS__, TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE,\
+		                TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE, ONE, throwaway)
+#define SELECT_10TH(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, ...) a10
 
 #define ARRAY_SIZE(arr) (sizeof (arr) / sizeof (arr[0]))
 
