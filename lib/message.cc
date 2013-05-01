ce3939591147d93d1065e0d7326effbeb9b6a292
Vladimir Marek <vlmarek@volny.cz>
2013-05-01 22:41:42 +0200

    lib/message.cc: stale pointer bug
    Xapian::TermIterator::operator* returns std::string which is destroyed
    as soon as (*i).c_str() finishes. The remembered pointer 'term' then
    references invalid memory.

--- lib/message.cc
+++ lib/message.cc
@@ -266,18 +266,19 @@ _notmuch_message_get_term (notmuch_message_t *message,
 			   const char *prefix)
 {
     int prefix_len = strlen (prefix);
-    const char *term = NULL;
+    std::string term;
     char *value;
 
     i.skip_to (prefix);
 
-    if (i != end)
-	term = (*i).c_str ();
-
-    if (!term || strncmp (term, prefix, prefix_len))
+    if (i == end)
 	return NULL;
 
-    value = talloc_strdup (message, term + prefix_len);
+    term = *i;
+    if (strncmp (term.c_str(), prefix, prefix_len))
+	return NULL;
+
+    value = talloc_strdup (message, term.c_str() + prefix_len);
 
 #if DEBUG_DATABASE_SANITY
     i++;
